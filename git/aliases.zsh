0c77577cdab62a4b1a306601c72df9113794e88c
Viktor Yakubiv <viktor@yakubiv.com>
2021-11-03 22:30:49 +0200

    Use custom aliases for Git

---
+++ git/aliases.zsh
@@ -0,0 +1,5 @@
+alias gbda="git branch --merged | egrep -v \"(^\*|master|dev)\" | xargs git branch -d"
+alias grp="git remote prune"
+alias grpo="git remote prune origin"
+alias gshn="git show --name-only"
+alias gbp="for b in \$(git for-each-ref --format='%(if:equals=[gone])%(upstream:track)%(then)%(refname:short)%(end)' refs/heads); do git branch -D \$b; done"
