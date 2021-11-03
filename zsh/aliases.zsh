b7ccfacd9be928695c0de290df751b818f13f033
Viktor Yakubiv <viktor@yakubiv.com>
2021-11-03 22:32:47 +0200

    Add global aliases for applications

---
+++ zsh/aliases.zsh
@@ -0,0 +1,10 @@
+# Quick app calls
+if [[ $OSTYPE == darwin* ]]; then
+  alias chrome="open -a \"Chrome Canary\""
+  alias firefox="open -a \"Firefox Developer Edition\""
+  alias merge="open -a \"Sublime Merge\""
+fi
+
+# Shortcuts for browsers
+alias ff="firefox"
+alias ch="chrome"
