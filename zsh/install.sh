7e3b6c51df9ba171e73fcc34b659f0f22d0933bf
Viktor Yakubiv <viktor@yakubiv.com>
2024-01-16 15:51:05 +0200

    Replace Spaceship Prompt with Starship

--- zsh/install.sh
+++ zsh/install.sh
@@ -8,8 +8,4 @@ ZSH_CUSTOM=${ZSH_CUSTOM:-"$ZSH/custom"}
 if [ ! -f "$OMZ_HOME/oh-my-zsh.sh" ]; then
   # Install ZSH if not installed
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-
-  # Install Spaceship Prompt
-  git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1 &&
-  ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 fi
