1c82feecc7e243287cd34a0e77931d271d310aee
Viktor Yakubiv <viktor@yakubiv.com>
2024-01-21 20:29:11 +0200

    Better Git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,7 +1,7 @@
-alias gai='git add --interactive'
-alias gbda="git branch --merged | egrep -v \"(^\*|master|dev)\" | xargs git branch -d"
+alias gbda="git branch --merged | egrep -v \"(^\*|master|main|dev)\" | xargs git branch -d"
 alias grp="git remote prune"
 alias grpo="git remote prune origin"
 alias gshn="git show --name-only"
+alias gshns="git show --name-status"
 alias gbp="for b in \$(git for-each-ref --format='%(if:equals=[gone])%(upstream:track)%(then)%(refname:short)%(end)' refs/heads); do git branch -D \$b; done"
 alias gst="git status -sb"
