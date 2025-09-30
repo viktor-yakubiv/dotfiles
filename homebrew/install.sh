840b03ac1cadf70f759014e9845e7dcd1209b09a
Viktor Yakubiv <viktor@yakubiv.com>
2025-09-30 11:38:13 +0300

    Install Homebrew on Linux too

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -10,11 +10,8 @@ if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
 
-  # Install the correct homebrew for each OS type
-  if test "$(uname)" = "Darwin"
-  then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  fi
+  # Install Homebrew on MacOS and Linux
+  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 exit 0
