c02b4527f32a519c0b8cd2b786e0f88c47bc3b77
Viktor Yakubiv <viktor@yakubiv.com>
2025-10-11 13:02:38 +0300

    Auto-link `~/.config/*` when installed else-where
    The installation directory cannot be controlled in the GitHub Codespace,
    so I need to include this kind of a workaround. It is also useful
    to have a location flexibility.

--- setup.sh
+++ setup.sh
@@ -8,3 +8,9 @@ git submodule update --init --recursive
 
 source "$DOTFILES_ROOT/.bin/bootstrap"
 source "$DOTFILES_ROOT/.bin/install"
+
+# Link .config
+# if installed into a different directory
+for dir in $(cat "$DOTFILES_ROOT/dot-config.lst"); do
+	ln -s "~/.config/$dir" "$DOTFILES_ROOT/$dir"
+done
