c9913c220f0aac3d214de47e8c80f8cff406b366
Viktor Yakubiv <viktor@yakubiv.com>
2025-10-11 13:00:41 +0300

    Auto-clone submodules on install
    Needed to automatically set everything up in dev containers,
    i.e. GitHub Codespaces.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 DOTFILES_ROOT="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+# Clone submodules
+# These include separated Neovim config and some dependencies
+git submodule update --init --recursive
+
 source "$DOTFILES_ROOT/.bin/bootstrap"
 source "$DOTFILES_ROOT/.bin/install"
