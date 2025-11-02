cc49c1a9e95dac9acc2c7667a7ed5283d605af10
Viktor Yakubiv <viktor@yakubiv.com>
2025-11-02 10:07:19 +0000

    Make Zgenom installation repeatable

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,8 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 #
 # ZSH installation
 #
 # This installs zgenom - a package manager for zsh
 # using Git, since it cannot be installed through Homebrew
-mkdir -p "$HOME/.local/share/zgenom"
-git clone https://github.com/jandamm/zgenom.git "$HOME/.local/share/zgenom"
+
+source "$(dirname "$(dirname "$(realpath "$0")")")/.lib/log.sh"
+
+ZGENOM_HOME="$HOME/.local/share/zgenom"
+
+mkdir -p "$ZGENOM_HOME"
+if [ -d "$ZGENOM_HOME" ]; then
+	info 'Zgenom seems to be installed. Skipping...'
+else
+	info "Clonning Zgenom to `$ZGENOM_HOME`"
+	git clone https://github.com/jandamm/zgenom.git "$ZGENOME_HOME"
+fi
