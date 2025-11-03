0ec959c92a1ccf05ce3bb9fab847fbb66f823804
Viktor Yakubiv <viktor@yakubiv.com>
2025-11-03 18:10:39 +0200

    Log events from the Homebrew installation script

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -8,8 +8,10 @@
 set -e
 
 SCRIPT_ROOT="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+source "$(dirname "$SCRIPT_ROOT")/.lib/log.sh"
 
 if test ! $(which brew); then
+	info "Homebrew was not found in PATH. Installing..."
 	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 	# Ensure PATH after the installation,
@@ -19,12 +21,14 @@ if test ! $(which brew); then
 	source "$SCRIPT_ROOT/env.zsh"
 fi
 
-# Brew core packages for all systems
+info "Brewing core packages for all systems"
 brew bundle --file="$SCRIPT_ROOT/core.brewfile"
 
 # Brew OS-specific packages
 if [ "$(uname -s)" == "Darwin" ]; then
+	info "Brewing MacOS-scpecific packages"
 	brew bundle --file="$SCRIPT_ROOT/macos.brewfile"
 elif [ "$(uname -s)" == "Linux" ]; then
+	info "Brewing Linux-scpecific packages"
 	brew bundle --file="$SCRIPT_ROOT/linux.brewfile"
 fi
