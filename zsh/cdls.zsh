2f03dc7850aaa71cb9a7f80a0ddcef6eefed3080
Viktor Yakubiv <viktor@yakubiv.com>
2025-11-03 17:39:16 +0200

    Anonymize cd-ls function

--- zsh/cdls.zsh
+++ zsh/cdls.zsh
@@ -1,8 +1,8 @@
 # Listing contents on `cd`
 
-function list_all() {
+function __list_all() {
   emulate -L zsh
   ls -A
 }
 
-chpwd_functions=(${chpwd_functions[@]} "list_all")
+chpwd_functions=(${chpwd_functions[@]} "__list_all")
